a0c3ec7a4e963b1b5ff82313bdb636ed18a2720b
virginia.pujols <virginia.pujols@edalsolutions.com>
2017-11-26 19:07:12 -0500

    PCC first screen. List Papers and PCMs

--- SAM2018/urls.py
+++ SAM2018/urls.py
@@ -6,10 +6,11 @@ from SAM2018 import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = ('SAM2018.views',
-                        url(r'^admin/', include(admin.site.urls)),
-                        url(r'^$', views.index, name='index'),
-                        url(r'^logout/$', auth_views.logout,{'next_page': 'index'}, name='logout'),
-                        url(r'^signup', views.signup, name='signup'),
-                        url(r'^paper/(?P<id>\d+)/$', views.view_paper, name='view_paper_url')
-                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+                  url(r'^admin/', include(admin.site.urls)),
+                  url(r'^$', views.index, name='index'),
+                  url(r'^logout/$', auth_views.logout,{'next_page': 'index'}, name='logout'),
+                  url(r'^signup', views.signup, name='signup'),
+                  url(r'^paper/(?P<id>\d+)/$', views.view_paper, name='view_paper_url'),
+                  url(r'^pcc/', include('pccDashboard.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pccDashboard/models.py
+++ pccDashboard/models.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from SAM2018.models import Review
 
-# Create your models here.
+
+class Report(models.Model):
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    comments = models.CharField(max_length=500, null=True)
+    rate = models.CharField(max_length=1, null=True)
+    reviews = models.ManyToManyField(
+        Review,
+        related_name='id',
+    )
