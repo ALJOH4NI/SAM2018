4baafea810ce18dbeea16f976c92975c6d8e1752
khaledalmalki <vb.95@hotmail.com>
2017-12-01 13:36:01 -0500

    fix urls

--- SAM2018/urls.py
+++ SAM2018/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from SAM2018 import views
+from SAM2018 import views, cPanel
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -12,4 +12,12 @@ urlpatterns = [
                   url(r'^logout/$', auth_views.logout,{'next_page': 'index'}, name='logout'),
                   url(r'^signup', views.signup, name='signup'),
                   url(r'^paper/(?P<id>\d+)/$', views.view_paper, name='view_paper_url'),
+                  url(r'^cpanel', cPanel.admin),
+                          url(r'^userMangament', cPanel.userMangament)
+                        ,  url(r'^deadlines', cPanel.deadlines)
+                        ,  url(r'^notifications', cPanel.notifications),
+                             url(r'^templates', cPanel.templates),
+                            url(r'^upadtaUser', cPanel.updateUser),
+                             url(r'^deleteUser', cPanel.deleteUser),
+                             url(r'^addNewUser', cPanel.addNewUser),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
